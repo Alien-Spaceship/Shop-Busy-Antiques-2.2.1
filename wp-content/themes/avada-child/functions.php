d444b2b0f18a2ab03cea051972904c20dd7c3e4f
Alien-Spaceship <mikediana330@gmail.com>
2016-02-23 12:24:45 -0500

    Update functions.php

--- wp-content/themes/avada-child/functions.php
+++ wp-content/themes/avada-child/functions.php
@@ -15,20 +15,9 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 add_filter('wp_nav_menu_items', 'add_login_logout_link', 10, 2); function add_login_logout_link($items, $args) {         ob_start();         wp_loginout('index.php');         $loginoutlink = ob_get_contents();         ob_end_clean();         $items .= '<li>'. $loginoutlink .'</li>';     return $items; }
 
 /**
- * Custom Admin Message.
+ * Custom Admin Message. //DELETED AFTER BRANCH//
  * Messages for logged in users.
  */
-add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
- 
-function my_custom_dashboard_widgets() {
-global $wp_meta_boxes;
- 
-wp_add_dashboard_widget('custom_help_widget', 'Admin Support', 'custom_dashboard_help');
-}
- 
-function custom_dashboard_help() {
-echo '<p><i class="fusion-li-icon fa fa-bullhorn" style="color:#262626;"></i> All new listings will be created and saved in "Pending". Click <a href="https://shopbusbyantiques.com/wp-admin/edit.php?post_status=pending&post_type=product" target="_blank">here</a> to see pending. Do not save as "Draft" Please use "Save as Pending" instead. Need help? For Tutorials visit: <a href="https://shopbusbyantiques.com/forums/topic/creating-new-listings/" target="_blank">Creating New Listings</a><br><i class="fusion-li-icon fa fa-check" style="color:#7ad03a;"></i> Agent1 updated: See your log sheet for new credentials</p>';
-}
 
 /*
  *
